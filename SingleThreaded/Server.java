a11633a70d211106e03ad2be9e81f20666aba85d
DESKTOP-KIQVIB2\Kuldeep singh <kuldeepm272@gmail.com>
2025-02-04 06:58:37 +0530

    Created socket Class connection and timeout

--- SingleThreaded/Server.java
+++ SingleThreaded/Server.java
@@ -1,6 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    public void run() throws IOException
+    {
+        int port=8010;
+        ServerSocket socket=new ServerSocket(port);
+        socket.setSoTimeout(10000);
+        while (true) {
+            try{
+                System.out.println("System is listening on port "+port);
+                Socket acceptConnection=socket.accept();
+                System.out.println("Connection accepted from client"+acceptConnection);
+                PrintWriter dataTOClient=new PrintWriter(acceptConnection.getOutputStream());
+                BufferedReader dataFromCLient=new BufferedReader(new InputStreamReader(acceptConnection.getInputStream()));
+                System.out.println("To client Hello from Server");
+
+            }catch(IOException ex){
+    
+                ex.printStackTrace();
+            }
+            
+        }
+       
+
+    }
     public static void main(String[] args) {
-        System.out.println("this is commit after rollBack");
+       Server server=new Server();
+       try{
+        server.run();
+       }catch(IOException ex)
+       {
+        ex.printStackTrace();
+       }
+       
     }
     
 }
